1251462ae8aae6ac2628fd10d85ecf45b9298910
jiggyjiggy <jiggyjiggy0323@gmail.com>
2023-04-23 20:03:03 +0900

    feat(book): 책 관리하기 페이지 생성

---
+++ src/test/java/jbro/librarysystem/unit/book/BookControllerTest.java
@@ -0,0 +1,26 @@
+package jbro.librarysystem.unit.book;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class BookControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void bookManagementRouter() throws Exception {
+        mockMvc.perform(get("/books/management"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("/books/managementRouter"));
+    }
+}
